6a6b4f9579c631235d92647771c20317e1ee5c7f
Martin-Coder-Cloud <martin.gravel@me.com>
2025-07-23 15:14:02 -0400

    Add files via upload

--- menu1/main.py
+++ menu1/main.py
@@ -83,7 +83,6 @@ def run_menu1():
         st.markdown("""
             <div class="custom-instruction">
                 Use this menu to explore results for a specific survey question.<br>
-                <br>
                 Select a question from the list below to begin or enter a keyword to search a question.
             </div>
         """, unsafe_allow_html=True)
