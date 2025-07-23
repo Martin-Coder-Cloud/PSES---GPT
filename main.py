c2301fed652266308cb59b1d2b389b56ee038cbc
Martin-Coder-Cloud <martin.gravel@me.com>
2025-07-23 14:33:18 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -68,9 +68,9 @@ def main():
                 background-color: transparent !important;
                 color: white !important;
                 border: 2px solid rgba(255, 255, 255, 0.3) !important;
-                font-size: 24px !important;
+                font-size: 30px !important;
                 font-weight: 600 !important;
-                padding: 18px 28px !important;
+                padding: 18px 30px !important;
                 width: 340px !important;
                 border-radius: 12px !important;
                 transition: 0.3s ease-in-out;
